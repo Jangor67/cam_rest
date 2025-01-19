a2cb0801045797d2e5aba0f057c0017650dd4674
Jan de Gorter <Jan@deGorter.net>
2025-01-19 21:23:40 +0100

    add preview

--- cam_rest.py
+++ cam_rest.py
@@ -1,11 +1,35 @@
 from flask import Flask, Response, request, jsonify
-from picamera2 import Picamera2
-import io
+from picamera2 import Picamera2, Preview
+import io, time
 
 app = Flask(__name__)
 picam = Picamera2()
-picam_config=picam.create_still_configuration()
-picam.configure(picam_config)
+
+still_config=picam.create_still_configuration()
+preview_config=picam.create_preview_configuration()
+
+is_started = False
+
+@app.route('/preview')
+def serve_preview():
+
+    global is_started
+    if is_started:
+        time.sleep(1)
+    if is_started:
+        return jsonify({'error': 'processing still in progress'}), 400
+    is_started = True
+
+    picam.configure(preview_config)
+    picam.start(show_preview=True)
+
+    stream = io.BytesIO()
+    picam.capture_file(stream, format='jpeg')
+    picam.stop()
+    is_started = False
+
+    stream.seek(0)
+    return Response(stream, mimetype='image/jpeg')
 
 @app.route('/image')
 def serve_image():
@@ -20,7 +44,7 @@ def serve_image():
             exposureTime = int(exposureTime)
             # Optionele controles op bereik
             # Pas bereik aan naar wat geschikt is voor je camera
-            if not (0 <= exposureTime <= 1000000):  
+            if not (0 <= exposureTime <= 2000000):
                 return jsonify({'error': 'exposureTime out of range'}), 400
             controls["ExposureTime"] = exposureTime
         except ValueError:
@@ -30,7 +54,7 @@ def serve_image():
         try:
             analogueGain = float(analogueGain)
             # Voorbeeld bereik, afhankelijk van je camera
-            if not (1.0 <= analogueGain <= 16.0):  
+            if not (1.0 <= analogueGain <= 16.0):
                 return jsonify({'error': 'analogueGain range is from 1.0 until 16.0 '}), 400
             controls["AnalogueGain"] = analogueGain
         except ValueError:
@@ -40,12 +64,20 @@ def serve_image():
         try:
             lensPosition = float(lensPosition)
             # Voorbeeld bereik, afhankelijk van je camera
-            if not (0.0 <= lensPosition <= 1.0):  
+            if not (0.0 <= lensPosition <= 1.0):
                 return jsonify({'error': 'lensPosition must be between 0.0 (far away) and 1.0 (closeby)'}), 400
             controls["LensPosition"] = lensPosition
         except ValueError:
             return jsonify({'error': 'lensPosition must be a float'}), 400
 
+    global is_started
+    if is_started:
+        time.sleep(1)
+    if is_started:
+        return jsonify({'error': 'processing still in progress'}), 400
+    is_started = True
+
+    picam.configure(still_config)
     if controls:
       try:
           picam.set_controls(controls)
@@ -53,9 +85,12 @@ def serve_image():
           return jsonify({'error': str(e)}), 500
 
     picam.start()
+
     stream = io.BytesIO()
     picam.capture_file(stream, format='jpeg')
     picam.stop()
+    is_started = False
+
     stream.seek(0)
     return Response(stream, mimetype='image/jpeg')
 
